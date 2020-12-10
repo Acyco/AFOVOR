cda37f31aca559993b1550c518c72056ed5ca579
Acyco <769225491@qq.com>
2020-12-10 14:47:39 +0800

    Remove Afovor-PHP module

--- Afovor-PHP/Afovor/Afovor.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * File: Afovor.php
- * User: Acyco
- * Date: 2020-12-01
- * Time: 08:26
- */
-
-echo "框架启动类";
--- Afovor-PHP/common/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * File: config.php
- * User: Acyco
- * Date: 2020-12-01
- * Time: 07:57
- */
