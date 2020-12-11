a91dc2e209373282966ebf96826023030dac2b0c
Acyco <769225491@qq.com>
2020-12-12 00:57:44 +0800

    Update frontend error page.

--- PHP/frontend/views/site/error.php
+++ PHP/frontend/views/site/error.php
@@ -5,23 +5,54 @@
 /* @var $message string */
 /* @var $exception Exception */
 
+use frontend\assets\PublicAsset;
 use yii\helpers\Html;
 
 $this->title = $name;
+PublicAsset::register($this);
 ?>
-<div class="site-error">
 
+
+<?php $this->beginPage()?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title><?= Html::encode($this->title) ?></title>
+
+    <?php $this->head()?>
+    <style>
+        .error-box h1{
+            font-size: 70px;
+        }
+    </style>
+</head>
+
+<body class="gray-bg">
+<?php $this->beginBody()?>
+
+<div class="middle-box text-center animated fadeInDown error-box">
     <h1><?= Html::encode($this->title) ?></h1>
+    <h3 class="font-bold"  <?= nl2br(Html::encode($message)) ?></h3>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+    <div class="error-desc">
+        抱歉，页面好像去火星了~ ?
+      <!--  <form class="form-inline m-t" role="form">
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="请输入您需要查找的内容 …">
+            </div>
+            <button type="submit" class="btn btn-primary">搜索</button>
+        </form>-->
+        
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
+<?php $this->endBody()?>
+</body>
+
+</html>
+<?php $this->endPage()?>
